e17f7197de169742f467620bb0f7fd055992b019
shiranamiworks <47758812+shiranamiworks@users.noreply.github.com>
2023-07-04 17:43:25 +0900

    Add files via upload

---
+++ sys/admin/css/torikago.php
@@ -0,0 +1,5 @@
+<?php
+  header('Content-Type: text/css; charset=utf-8');
+  include_once( dirname(__FILE__).'/../../setting/css/_design.css');
+  include_once( dirname(__FILE__).'/../../setting/css/torikago_admin.css');
+?>
